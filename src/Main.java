5696571855b4ed0c6d7e7670acd8ce685579c90e
Empoliam <liamjwright@btinternet.com>
2018-04-09 18:29:51 +0100

    Can create animated graphics now

--- src/Main.java
+++ src/Main.java
@@ -14,8 +14,8 @@ public class Main {
 	static public double YMAX = 3.0;
 
 	//Must be whole numbers
-	static public double XRES = 4000;
-	static public double YRES = 4000;
+	static public double XRES = 1000;
+	static public double YRES = 1000;
 
 	static boolean ZGRID = false;
 	static int ZGRIDDIV = 1;
@@ -28,7 +28,7 @@ public class Main {
 
 	static final Complex ONE = new Complex(1,0);
 	static final Complex I = new Complex(0,1);
-	
+
 	public static void main(String[] args) {
 
 		double xbase = (XMAX - XMIN) / XRES;
@@ -37,49 +37,59 @@ public class Main {
 		double originx = XRES / 2;
 		double originy = YRES / 2;
 
-		for(int x = 0; x < XRES; x ++) {
+		for (int frame = 0; frame < 90; frame++) {
 
-			for(int y = 0; y < YRES; y++) {
+			for (int x = 0; x < XRES; x++) {
 
-				Complex Z = new Complex((x-originx)*(double)xbase,(y-originy)*(double)ybase);
+				for (int y = 0; y < YRES; y++) {
 
-				Complex J = Z.add(new Complex(1,0));
-				Complex K = Z.add(new Complex(-1,0));
-				Complex L = Z.add(new Complex(0, 1));
-				Complex M = Z.add(new Complex(0,-1));
-				
-				Complex W = I.multiply(Z);
+					Complex Z = new Complex((x - originx) * (double) xbase, (y - originy) * (double) ybase);
 
-				double s = MoreMath.logAB(2.0, W.abs());
-				if(s > 0) s = s%1;
-				else s = 1-(Math.abs(s)%1);
-				
-				s = s*0.7 + 0.3;
-				
-				Color D = Color.getHSBColor((float)(W.twoPiArg()/(Math.PI*2)), (float)s, (float)1.0);
+					Complex J = Z.add(new Complex(0.0+(frame/90.0), 0));
+					Complex K = Z.add(new Complex(0.0-(frame/90.0), 0));
 
-				output.setRGB(x, (int)YRES-1-y, D.getRGB());
+					Complex W = J.multiply(K);
 
-				if(ZGRID) {
-					if(Math.abs(Z.r)%ZGRIDDIV < 0.01)	output.setRGB(x, (int)YRES-1-y, 0);		
-					else if(Math.abs(Z.i)%ZGRIDDIV < 0.01)	output.setRGB(x, (int)YRES-1-y, 0);	
-				}
+					double s = MoreMath.logAB(2.0, W.abs());
+					if (s > 0)
+						s = s % 1;
+					else
+						s = 1 - (Math.abs(s) % 1);
 
-				if(WGRID) {
-					double modr = Math.abs(W.r)%WGRIDDIV;
-					double modi = Math.abs(W.i)%WGRIDDIV;
-					if(modr < WGRIDTOLERANCE)	output.setRGB(x, (int)YRES-1-y, 0);		
-					else if(modi < WGRIDTOLERANCE) output.setRGB(x, (int)YRES-1-y, 0);	
+					s = s * 0.7 + 0.3;
+
+					Color D = Color.getHSBColor((float) (W.twoPiArg() / (Math.PI * 2)), (float) s, (float) 1.0);
+
+					output.setRGB(x, (int) YRES - 1 - y, D.getRGB());
+
+					if (ZGRID) {
+						if (Math.abs(Z.r) % ZGRIDDIV < 0.01)
+							output.setRGB(x, (int) YRES - 1 - y, 0);
+						else if (Math.abs(Z.i) % ZGRIDDIV < 0.01)
+							output.setRGB(x, (int) YRES - 1 - y, 0);
+					}
+
+					if (WGRID) {
+						double modr = Math.abs(W.r) % WGRIDDIV;
+						double modi = Math.abs(W.i) % WGRIDDIV;
+						if (modr < WGRIDTOLERANCE)
+							output.setRGB(x, (int) YRES - 1 - y, 0);
+						else if (modi < WGRIDTOLERANCE)
+							output.setRGB(x, (int) YRES - 1 - y, 0);
+					}
+
 				}
 
 			}
-
+			File f = new File("output" + frame  + ".png");
+			try {
+				ImageIO.write(output, "PNG", f);
+			} catch (IOException e) {
+				System.out.println("Failed to print");
+			};
+			
 		}
 
-		File f = new File("output.png");
-		try { ImageIO.write(output, "PNG", f); }
-		catch(IOException e){System.out.println("Failed to print"); };
-					
 	}
 
 }
